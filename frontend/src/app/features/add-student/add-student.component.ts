6bfc6257a75ebd4ef89ebc5f8ce9e13a5c977778
rhaneyko <rhaneykohonorio@gmail.com>
2025-01-05 20:09:09 -0400

    Feito a função de adicionar novo estudante.

--- frontend/src/app/features/add-student/add-student.component.ts
+++ frontend/src/app/features/add-student/add-student.component.ts
@@ -8,28 +8,17 @@ import { Student } from '../../model/student'; // Importe do arquivo correto
   styleUrls: ['./add-student.component.css']
 })
 export class AddStudentComponent {
-  name: string = '';
-  email: string = '';
-  course: string = '';
+  students: any[] = [];
+  newStudent = { name: '', email: '', course: ''};
 
   constructor(private studentService: StudentService) {}
 
   addStudent() {
-    if (this.name && this.email && this.course) {
-      const newStudent: Student = {
-        id: Math.floor(Math.random() * 1000),
-        name: this.name,
-        email: this.email,
-        course: this.course,
-        registration: Math.floor(Math.random() * 1000000).toString()
-      };
-
-      this.studentService.addStudent(newStudent);
-      this.name = '';
-      this.email = '';
-      this.course = '';
-    } else {
-      alert('Por favor, preencha todos os campos!');
-    }
+    this.studentService.addStudent(this.newStudent).subscribe(() => {
+      this.students.push(this.newStudent);
+      this.newStudent = { name: '', email: '', course: '' };
+    });
   }
+
+
 }
